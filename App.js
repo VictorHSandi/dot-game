e604fe655029b8088788177105e063a513b3e723
KPeters <99045807+kpe03@user.noreply.github.com>
2024-03-17 18:14:25 -0500

    added react navigation

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 import React, { useState } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import dot from './assets/dots.png'
 
 export default function App() {

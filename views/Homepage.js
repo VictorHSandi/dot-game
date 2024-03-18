12dd354363e1b7e79fb9952d45ed11a31b836653
KPeters <99045807+kpe03@user.noreply.github.com>
2024-03-17 20:27:05 -0500

    Got fonts to work

--- views/Homepage.js
+++ views/Homepage.js
@@ -1,21 +1,40 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native';
+import {useFonts} from 'expo-font';
+import * as Font from 'expo-font';
 
 //landing page
 export default function Homepage() {
-    return(
-        <View>
-         <Text style={styles.title}>Homepage</Text>
-        </View>
-    );
+    //fonts stuff
+    let customFonts = {
+            'MadimiOne-Regular': require('../assets/font/MadimiOne-Regular.ttf')
+    };
+    //hook for custom fonts. when they are loaded, the page will load
+    const[isLoaded] = useFonts(customFonts);
+
+    //view
+    if(isLoaded) {
+        return(
+            <View>
+            <Text style={styles.title}>Dot Game</Text>
+            </View>
+        );
+    } 
 }
+
 
 const styles = StyleSheet.create({
     title: {
         fontSize: 40,
-        fontFamily: 'Roboto',
+        fontFamily: 'MadimiOne-Regular',
         color: '#1A1616'
     },
     container: {
         backgroundColor: '#D9D9D9'
+    },
+    madimiOneRegular: {
+        fontFamily: 'MadimiOne-Regular',
+        fontWeight: 'normal',
+        fontStyle: 'normal',
     }
+      
 });
